eb922a7aa365daa9ecc363e7bec4c7ea7c678f01
Mia Rødde <miarodde@Mias-MBP.sintef.no>
2023-08-30 10:45:50 +0200

    bug: modified plot_predictions so it worked for multiclass model as well

--- src/visualization/prediction_model.py
+++ src/visualization/prediction_model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 def get_predictions(model, test_ds):
     probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
@@ -10,17 +11,24 @@ def get_predictions(model, test_ds):
 def plot_predictions(model, test_ds, station_names, reports_path):
     print('All predictions: ')
     print(get_predictions(model, test_ds))
+    print('station names: ', station_names)
 
     # plot the first 25 images in the test set, and their predicted labels and true labels
     fig = plt.figure(figsize=(10, 10))
     for images, labels in test_ds.take(1):
         predictions = get_predictions(model, images)
         for i in range(9):
+            print('predictions[i]: ', predictions[i])
+            print('np.argmax(labels[i]): ', np.argmax(labels[i]))
             ax = plt.subplot(3, 3, i + 1)
             # normalize image from range [-1, 1] to [0, 1]
             image = (images[i] + 1) / 2
             plt.imshow(image)
-            plt.title("Predicted: {}, True: {}".format(station_names[int(predictions[i][0])],
+            if len(station_names) > 2:
+                plt.title("Predicted: {}, True: {}".format(station_names[np.argmax(predictions[i])],
+                                                           station_names[np.argmax(labels[i])]))
+            else:
+                plt.title("Predicted: {}, True: {}".format(station_names[int(predictions[i][0])],
                                                        station_names[labels.numpy()[i]]))
             plt.axis("off")
 
