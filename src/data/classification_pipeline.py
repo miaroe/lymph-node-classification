c3452cc1bcc781b0d501f91d7b9e9b9bcc5e7413
Mia Rødde <miarodde@Mias-MBP.sintef.no>
2023-11-07 10:39:15 +0100

    perf: added augmentation, split train and val sets

--- src/data/classification_pipeline.py
+++ src/data/classification_pipeline.py
@@ -2,13 +2,11 @@ import logging
 import tensorflow as tf
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-from alive_progress import alive_bar, config_handler
 from sklearn.model_selection import train_test_split
 import random
 
-from src.resources.augmentation import GammaTransform, ContrastScale, Blur, BrightnessTransform, GaussianShadow, RandomAugmentation
-from src.data.full_video_label_dict import get_frame_label_dict
+from src.resources.augmentation import GammaTransform, ContrastScale, Blur, BrightnessTransform, GaussianShadow, \
+    RandomAugmentation, Rotation, NonLinearMap
 from src.utils.get_paths import get_station_paths, get_frame_paths
 
 log = logging.getLogger()
@@ -20,8 +18,7 @@ log = logging.getLogger()
 class ClassificationPipeline:
 
     def __init__(self, data_path, test_ds_path, batch_size, image_shape, validation_split, test_split, station_names,
-                 num_stations, augment,
-                 shuffle):
+                 num_stations, augment):
         self.data_path = data_path
         self.test_ds_path = test_ds_path
         self.batch_size = batch_size
@@ -31,18 +28,25 @@ class ClassificationPipeline:
         self.validation_split = validation_split
         self.test_split = test_split
         self.augment = augment
-        self.shuffle = shuffle
         self.train_ds = None
         self.val_ds = None
         self.test_ds = None
+
+
+
+
+class BaselineClassificationPipeline(ClassificationPipeline):
+
+    def __init__(self, data_path, test_ds_path, batch_size, image_shape, validation_split, test_split, station_names,
+                 num_stations, augment):
+        super().__init__(data_path, test_ds_path, batch_size, image_shape, validation_split, test_split, station_names,
+                         num_stations, augment)
 
     def loader_function(self):
         print(self.station_names)
 
         train_ds = tf.keras.utils.image_dataset_from_directory(
-            directory=self.data_path,
-            validation_split=self.validation_split,
-            subset='training',
+            directory=os.path.join(self.data_path, 'train'),
             seed=123,
             labels='inferred',
             label_mode=self.get_label_mode(),
@@ -50,58 +54,61 @@ class ClassificationPipeline:
             class_names=self.station_names,
             batch_size=self.batch_size,
             image_size=self.image_shape,
-            shuffle=self.shuffle
+            shuffle=True
         )
 
         val_ds = tf.keras.utils.image_dataset_from_directory(
-            directory=self.data_path,
-            validation_split=self.validation_split,
-            subset='validation',
-            seed=123,
+            directory=os.path.join(self.data_path, 'val'),
             labels='inferred',
             label_mode=self.get_label_mode(),
             color_mode='rgb',
             class_names=self.station_names,
             batch_size=self.batch_size,
             image_size=self.image_shape,
-            shuffle=self.shuffle
+            shuffle=False
         )
 
         test_ds = tf.keras.utils.image_dataset_from_directory(
             directory=self.test_ds_path,
-            seed=123,
             labels='inferred',
             label_mode=self.get_label_mode(),
             color_mode='rgb',
             class_names=self.station_names,
             batch_size=self.batch_size,
             image_size=self.image_shape,
-            shuffle=self.shuffle
+            shuffle=False
         )
+
 
         self.train_ds = self.prepare(train_ds, augment=self.augment)
         self.val_ds = self.prepare(val_ds, augment=False)  # no augmentation for validation and test data
         self.test_ds = self.prepare(test_ds, augment=False)
 
+        '''
         # Used to test operations
-        '''
-        image_batch, labels_batch = next(iter(train_ds))
+        image_batch, labels_batch = next(iter(self.val_ds))
         first_image = image_batch[0]
-        print(np.min(first_image), np.max(first_image))
+        print('first image: ', first_image)
+        print('min: ', np.min(first_image), 'max: ', np.max(first_image))
+        print('image ', first_image)
         plt.figure(figsize=(10, 10))
-        for images, labels in self.train_ds.take(1):
-            for i in range(9):
-                ax = plt.subplot(3, 3, i + 1)
-                plt.imshow(images[i])
-                plt.axis("off")
-            plt.show()
+        #first_image = (first_image + 1) / 2
+        plt.imshow(first_image)
+        plt.title('training' + str(labels_batch[0]))
+        plt.show()
         '''
+
+
+
 
         return self.train_ds, self.val_ds, self.test_ds
 
+    #TODO: remove
+    '''
     def crop_image(self, image):
         cropped_image = tf.image.crop_to_bounding_box(image, 24, 71, 223, 150)
         return cropped_image
+    '''
 
     def get_label_mode(self):
         if self.num_stations > 2:
@@ -116,47 +123,40 @@ class ClassificationPipeline:
         # https://www.tensorflow.org/tutorials/load_data/images
         # https://www.tensorflow.org/tutorials/images/data_augmentation
 
+        rescale = tf.keras.Sequential([
+            # tf.keras.layers.Rescaling(1. / 127.5, offset=-1)  # specific for mobilenet TODO: change for other models
+            tf.keras.layers.Rescaling(1. / 255.0)
+        ])
+        '''
         data_augmentation = tf.keras.Sequential([
             tf.keras.layers.RandomRotation(0.1),  # rotating by a random amount in the range [-10% * 2pi, 10% * 2pi]
             tf.keras.layers.RandomContrast(0.1)  # (x - mean) * contrast_factor + mean
         ])
+        '''
 
-        resize_and_rescale = tf.keras.Sequential([
-            tf.keras.layers.Resizing(256, 256),
-            tf.keras.layers.Rescaling(1. / 127.5, offset=-1)  # specific for mobilenet TODO: change for other models
-            # tf.keras.layers.Rescaling(1. / 255.0)
 
-        ])
-        # Apply cropping
-        ds = ds.map(lambda x, y: (self.crop_image(x), y), num_parallel_calls=AUTOTUNE)
-        # Resize and rescale all datasets.
-        ds = ds.map(lambda x, y: (resize_and_rescale(x), y), num_parallel_calls=AUTOTUNE)
+        augmentation_layers = [GammaTransform(low=0.5, high=1.5), Blur(sigma_max=1.5), NonLinearMap()]
+                               #Rotation(max_angle=30), GaussianShadow(sigma_x=(0.1, 0.5), sigma_y=(0.1, 0.9), strength=(0.5, 0.8))]
+        data_augmentation = RandomAugmentation(augmentation_layers)
 
-        # Use data augmentation only on the training set.
+        # Rescale all datasets.
+        ds = ds.map(lambda x, y: (rescale(x), y), num_parallel_calls=AUTOTUNE)
+
         if augment:
-            ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y),
-                        num_parallel_calls=AUTOTUNE)
+            # Use data augmentation only on the training set.
+            #ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y), num_parallel_calls=AUTOTUNE)
+            #ds = ds.map(lambda x, y: (data_augmentation(x), y), num_parallel_calls=AUTOTUNE)
+            ds = ds.map(lambda x, y: (tf.py_function(func=data_augmentation, inp=[x], Tout=tf.float32), y), num_parallel_calls=tf.data.AUTOTUNE)
+
         # Use buffered prefetching on all datasets.
         return ds.prefetch(buffer_size=AUTOTUNE)
 
 
-class BaselineClassificationPipeline(ClassificationPipeline):
-
-    def __init__(self, data_path, test_ds_path, batch_size, image_shape, validation_split, test_split, station_names,
-                 num_stations, augment, shuffle):
-        super().__init__(data_path, test_ds_path, batch_size, image_shape, validation_split, test_split, station_names,
-                         num_stations, augment, shuffle)
-
-    def loader_function(self):
-        self.train_ds, self.val_ds, self.test_ds = super().loader_function()
-        return self.train_ds, self.val_ds, self.test_ds
-
-
 class SequenceClassificationPipeline(ClassificationPipeline):
     def __init__(self, data_path, test_ds_path, batch_size, image_shape, validation_split, test_split, station_names,
-                 num_stations, augment, shuffle, stations_config, seq_length, stride):
+                 num_stations, augment, stations_config, seq_length, stride):
         super().__init__(data_path, test_ds_path, batch_size, image_shape, validation_split, test_split, station_names,
-                         num_stations, augment, shuffle)
+                         num_stations, augment)
         self.seq_length = seq_length
         self.stride = stride
         self.stations_config = stations_config
@@ -185,11 +185,11 @@ class SequenceClassificationPipeline(ClassificationPipeline):
 
     def load_image(self, image_path):
         """Load and preprocess a single image from a file path"""
-        img = tf.io.read_file(image_path)
-        img = tf.image.decode_image(img, channels=3)  # Assuming RGB images
+        img = tf.keras.utils.load_img(image_path, color_mode='rgb', target_size=None)
+        img = tf.cast(img, tf.float32)  # pixel values are in range [0, 255] to apply preprocessing layer in ml_models
         img = img[100:1035, 530:1658]  # Cropping the image to the region of interest
-        img = tf.cast(img, tf.float32) # pixel values are in range [0, 255] to apply preprocessing layer in ml_models
         #img = (tf.cast(img, tf.float32) / 127.5) - 1  # specific for mobilenet, inception TODO: change for other models
+        #img = img / 255.0 # normalizing the image to be in range [0, 1] #TODO: change for other models
         img = tf.image.resize(img, self.image_shape)  # resizing the image to the desired shape
         return img
 
@@ -265,7 +265,7 @@ class SequenceClassificationPipeline(ClassificationPipeline):
         '''
 
         if self.augment:
-            augmentation_layers = [GammaTransform(), ContrastScale(), Blur(), BrightnessTransform(),
+            augmentation_layers = [GammaTransform(), ContrastScale(), Blur(), BrightnessTransform(), NonLinearMap(),
                                    GaussianShadow(sigma_x=(0.1, 0.5), sigma_y=(0.1, 0.9), strength=(0.5, 0.8))]
             data_augmentation = RandomAugmentation(augmentation_layers)
 
@@ -274,7 +274,7 @@ class SequenceClassificationPipeline(ClassificationPipeline):
                                                                  inp=[x], Tout=tf.float32), y),
                                     num_parallel_calls=tf.data.AUTOTUNE)
 
-        # shuffle and batch the datasets
+        # batch and prefetch the datasets
         self.train_ds = train_ds.batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
         self.val_ds = val_ds.batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
 
--- src/resources/augmentation_tf.py
+++ src/resources/augmentation_tf.py
@@ -1,7 +1,5 @@
-import numpy as np
+import tensorflow as tf
 from tensorflow.keras import layers
-from scipy.ndimage import gaussian_filter
-from scipy.ndimage import rotate
 
 
 class RandomAugmentation(layers.Layer):
@@ -12,7 +10,7 @@ class RandomAugmentation(layers.Layer):
 
     def call(self, data, **kwargs):
         for augmentation_layer in self.augmentation_layers:
-            apply_augmentation = np.random.random() < self.probability
+            apply_augmentation = tf.random.uniform(shape=(), dtype=tf.float32) < self.probability
             #print('apply_augmentation: ', apply_augmentation)
             if apply_augmentation:
                 augmentation_layer.randomize()
@@ -28,11 +26,12 @@ class GammaTransform(layers.Layer):
         self._random_gamma = None
 
     def randomize(self):
-        self._random_gamma = np.random.uniform(self.low, self.high)
+        self._random_gamma = tf.random.uniform(shape=(), minval=self.low, maxval=self.high)
 
     def call(self, data, **kwargs):
+        #self.randomize()
         #print('GammaTransform: ', self._random_gamma)
-        return np.clip(np.power(data, self._random_gamma), 0, 1)
+        return tf.clip_by_value(tf.pow(data, self._random_gamma), -1, 1)
 
 
 class ContrastScale(layers.Layer):
@@ -43,11 +42,12 @@ class ContrastScale(layers.Layer):
         self._random_scale = None
 
     def randomize(self):
-        self._random_scale = np.random.uniform(self.min_scale, self.max_scale)
+        self._random_scale = tf.random.uniform(shape=(), minval=self.min_scale, maxval=self.max_scale)
 
     def call(self, data, **kwargs):
+        #self.randomize()
         #print('ContrastScale: ', self._random_scale)
-        return np.clip(data * self._random_scale, 0, 1)
+        return tf.clip_by_value(data * self._random_scale, -1, 1)
 
 
 class Blur(layers.Layer):
@@ -58,11 +58,12 @@ class Blur(layers.Layer):
         self._random_sigma = None
 
     def randomize(self):
-        self._random_sigma = np.random.uniform(self.sigma_min, self.sigma_max)
+        self._random_sigma = tf.random.uniform(shape=(), minval=self.sigma_min, maxval=self.sigma_max)
 
     def call(self, data, *args, **kwargs):
+        #self.randomize()
         #print('Blur: ', self._random_sigma)
-        return gaussian_filter(data, self._random_sigma) * np.array(data > 0)
+        return tf.clip_by_value(tf.image.gaussian_filter2d(data, [3, 3], self._random_sigma), -1, 1) # not sure about this
 
 
 class BrightnessTransform(layers.Layer):
@@ -72,11 +73,12 @@ class BrightnessTransform(layers.Layer):
         self._random_scale = None
 
     def randomize(self):
-        self._random_scale = np.random.uniform(-self.max_scale, self.max_scale)
+        self._random_scale = tf.random.uniform(shape=(), minval=-self.max_scale, maxval=self.max_scale)
 
     def call(self, data, **kwargs):
+        #self.randomize()
         #print('BrightnessTransform: ', self._random_scale)
-        return np.clip(data + self._random_scale, 0, 1)
+        return tf.clip_by_value(data + self._random_scale, -1, 1)
 
 
 class GaussianShadow(layers.Layer):
@@ -114,6 +116,7 @@ class GaussianShadow(layers.Layer):
         self._random_location = np.random.uniform(-1.0, 1.0, 2).astype(np.float32)
 
     def call(self, data, return_map=False, **kwargs):
+        #self.randomize()
         #print('GaussianShadow: ', self._random_sigma_x, self._random_sigma_y, self._random_strength, self._random_location)
         x, y = np.meshgrid(np.linspace(-1, 1, data.shape[0], dtype=np.float32),
                            np.linspace(-1, 1, data.shape[1], dtype=np.float32), copy=False, indexing='ij')
@@ -135,84 +138,3 @@ class GaussianShadow(layers.Layer):
         if return_map:
             return augmented_data, g
         return augmented_data
-
-
-class Rotation(layers.Layer):
-    """
-    Rotates image with arbitrary angle in bounds [-max_angle:max_angle]
-
-    Parameters
-    ----------
-    max_angle: number
-        Maximum angle in degrees.
-    """
-
-    def __init__(self, max_angle=15, flow_indices=None, segmentation_indices=None, **kwargs):
-        super().__init__(**kwargs)
-        self.max_angle = max_angle
-        self._random_angle = None
-        self.flow_indices = flow_indices
-        self.segmentation_indices = segmentation_indices
-
-    def call(self, data, data_index=None, *args, **kwargs):
-        #print('Rotation: ', self._random_angle)
-        data = np.copy(data)
-
-        if len(data.shape) > 2:
-            for channel in range(0, data.shape[2]):
-                if self.segmentation_indices is not None and data_index in self.segmentation_indices:
-                    cval = find_best_padding_cval(data[..., channel])
-                    data[..., channel] = rotate(data[..., channel], self._random_angle, order=0, reshape=False,
-                                                cval=cval)
-                else:
-                    data[..., channel] = rotate(data[..., channel], self._random_angle, order=1, reshape=False)
-        else:
-            data = rotate(data, self._random_angle, order=1, reshape=False)
-
-        if self.flow_indices is not None and data_index in self.flow_indices:
-            theta = np.radians(-self._random_angle)
-            c, s = np.cos(theta), np.sin(theta)
-            rot_mat = np.array(((c, -s), (s, c)))
-            data = data.dot(rot_mat)
-        return data
-
-    def randomize(self):
-        self._random_angle = np.random.randint(-self.max_angle, self.max_angle)
-
-class NonLinearMap(layers.Layer):
-    def __init__(self, alpha=0.544559, beta=1.686562, gamma=5.598193, delta=0.638681, y0=0.002457387314, **kwargs):
-        super().__init__(**kwargs)
-
-        self.alpha = alpha
-        self.beta = beta
-        self.gamma = gamma
-        self.delta = delta
-        self.y0 = y0
-
-
-    def call(self, data, *args, **kwargs):
-        #print('NonLinearMap: ', self.alpha, self.beta, self.gamma, self.delta, self.y0)
-        data = self.alpha * np.exp(-np.exp(self.beta - self.gamma * data) + self.delta * data) - self.y0
-        return np.clip(data, a_min=0, a_max=1)
-
-    def randomize(self):
-        pass
-
-def find_best_padding_cval(segmentation_mask):
-    """
-    To be used geometrical transformations (scale, rotation, translation...) on segmentation masks.
-    Indeed, using filling modes like 'nearest' will lead to irrealistic shapes in case the masked structure
-    (background, left_ventricle, left-atrium...) is at the border of the frame. Padding with 'constant' to 0 is not a
-    solution either since we want to fill with continuity with the background channel (which is mostly on the borders
-    of the image).
-    This function finds the best fill value for each channel (value which is majority at the four images edges) of the
-    segmentation mask.
-    """
-    edge1 = segmentation_mask[0, :-1]
-    edge2 = segmentation_mask[:-1, -1]
-    edge3 = segmentation_mask[-1, 1:]
-    edge4 = segmentation_mask[1:, 0]
-    average_edge = np.mean(np.concatenate([edge1, edge2, edge3, edge4]))
-    return np.round(average_edge).astype(segmentation_mask.dtype)
-
-
