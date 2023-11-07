54b4a2cf9a4360d9300dd5932989cb34922dd1c2
Mia Rødde <miarodde@Mias-MBP.sintef.no>
2023-11-07 10:46:13 +0100

    fix: minor changes

--- src/visualization/visual_model.py
+++ src/visualization/visual_model.py
@@ -1,9 +1,13 @@
 import visualkeras
-from PIL import ImageFont
 import os
 
-
 def visual_model(model, reports_path):
+    """
+    Visualize the model architecture using visualkeras
+    :param model:
+    :param reports_path:
+    :return:
+    """
     fig_name = 'visual_model.png'
     fig_path = os.path.join(reports_path, 'figures/')
     os.makedirs(fig_path, exist_ok=True)
